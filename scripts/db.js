33a819a2c9029fbbfbe8021d735017e48e63b308
wanjamueller <me@wanjamueller.com>
2026-07-22 21:11:29 +0200

    JSOn created and placeholder images uploaded and linked

---
+++ scripts/db.js
@@ -0,0 +1,218 @@
+let dishes = [
+    {
+        "name": "Classic BBQ Bacon Burger",
+        "description":
+            "Beef patty smothered in smoky BBQ sauce, crispy bacon, cheddar cheese, and crunchy onion straws on a toasted brioche bun.",
+        "img": "../assets/img/dishes/classic-bbq-bacon-burger.jpg",
+        "price": 12.49,
+        "category": "Burger & Sandwiches",
+        "amount": "0",
+    },
+    {
+        "name": "Smoky Pulled Pork Sandwich",
+        "description":
+            "Slow-smoked pulled pork tossed in tangy BBQ sauce, topped with creamy coleslaw on a toasted kaiser roll.",
+        "img": "../assets/img/dishes/smoky-pulled-pork-sandwich.jpg",
+        "price": 11.99,
+        "category": "Burger & Sandwiches",
+        "amount": "0",
+    },
+    {
+        "name": "All-American Cheeseburger",
+        "description":
+            "Juicy beef patty, melted American cheese, lettuce, tomato, pickles, and house sauce on a soft sesame bun.",
+        "img": "../assets/img/dishes/all-american-cheeseburger.jpg",
+        "price": 10.99,
+        "category": "Burger & Sandwiches",
+        "amount": "0",
+    },
+    {
+        "name": "Grilled Chicken Club Sandwich",
+        "description":
+            "Grilled chicken breast, crispy bacon, Swiss cheese, lettuce, and tomato stacked on toasted sourdough with mayo.",
+        "img": "../assets/img/dishes/grilled-chicken-club-sandwich.jpg",
+        "price": 11.49,
+        "category": "Burger & Sandwiches",
+        "amount": "0",
+    },
+    {
+        "name": "Texas BBQ Brisket Burger",
+        "description":
+            "Slow-smoked beef brisket, tangy BBQ glaze, crispy fried onions, and pepper jack cheese on a pretzel bun.",
+        "img": "../assets/img/dishes/texas-bbq-brisket-burger.jpg",
+        "price": 13.99,
+        "category": "Burger & Sandwiches",
+        "amount": "0",
+    },
+    {
+        "name": "Classic Pepperoni Pizza",
+        "description":
+            "Hand-tossed crust topped with zesty tomato sauce, mozzarella cheese, and generous pepperoni slices.",
+        "img": "../assets/img/dishes/classic-pepperoni-pizza.jpg",
+        "price": 13.99,
+        "category": "Pizza",
+        "amount": "0",
+    },
+    {
+        "name": "BBQ Chicken Pizza",
+        "description":
+            "Smoky BBQ sauce base with grilled chicken, red onions, mozzarella, and a drizzle of extra BBQ sauce.",
+        "img": "../assets/img/dishes/bbq-chicken-pizza.jpg",
+        "price": 14.99,
+        "category": "Pizza",
+        "amount": "0",
+    },
+    {
+        "name": "Margherita Pizza",
+        "description": "Fresh mozzarella, ripe tomatoes, and basil over a light tomato sauce on a crispy thin crust.",
+        "img": "../assets/img/dishes/margherita-pizza.jpg",
+        "price": 12.99,
+        "category": "Pizza",
+        "amount": "0",
+    },
+    {
+        "name": "Meat Lovers Pizza",
+        "description":
+            "Loaded with pepperoni, Italian sausage, bacon, and ham over melted mozzarella and tomato sauce.",
+        "img": "../assets/img/dishes/meat-lovers-pizza.jpg",
+        "price": 15.99,
+        "category": "Pizza",
+        "amount": "0",
+    },
+    {
+        "name": "Hawaiian Pizza",
+        "description": "Sweet pineapple, smoked ham, and mozzarella cheese over a classic tomato sauce base.",
+        "img": "../assets/img/dishes/hawaiian-pizza.jpg",
+        "price": 13.49,
+        "category": "Pizza",
+        "amount": "0",
+    },
+    {
+        "name": "Classic Caesar Salad",
+        "description": "Crisp romaine lettuce, parmesan cheese, garlic croutons, and creamy Caesar dressing.",
+        "img": "../assets/img/dishes/classic-caesar-salad.jpg",
+        "price": 8.99,
+        "category": "Salads",
+        "amount": "0",
+    },
+    {
+        "name": "Grilled Chicken Cobb Salad",
+        "description":
+            "Grilled chicken, crispy bacon, hard-boiled egg, avocado, blue cheese, and tomatoes over mixed greens.",
+        "img": "../assets/img/dishes/grilled-chicken-cobb-salad.jpg",
+        "price": 10.99,
+        "category": "Salads",
+        "amount": "0",
+    },
+    {
+        "name": "BBQ Ranch Chopped Salad",
+        "description":
+            "Grilled chicken, black beans, corn, cheddar cheese, and crispy tortilla strips tossed in BBQ ranch dressing.",
+        "img": "../assets/img/dishes/bbq-ranch-chopped-salad.jpg",
+        "price": 10.49,
+        "category": "Salads",
+        "amount": "0",
+    },
+    {
+        "name": "Southwest Salad",
+        "description":
+            "Mixed greens with grilled chicken, black beans, corn, pico de gallo, and chipotle lime dressing.",
+        "img": "../assets/img/dishes/southwest-salad.jpg",
+        "price": 10.49,
+        "category": "Salads",
+        "amount": "0",
+    },
+    {
+        "name": "Garden Salad",
+        "description":
+            "Fresh mixed greens, cucumbers, cherry tomatoes, shredded carrots, and red onion with your choice of dressing.",
+        "img": "../assets/img/dishes/garden-salad.jpg",
+        "price": 7.99,
+        "category": "Salads",
+        "amount": "0",
+    },
+    {
+        "name": "Classic Vanilla Milkshake",
+        "description": "Rich and creamy vanilla ice cream blended to smooth perfection, topped with whipped cream.",
+        "img": "../assets/img/dishes/classic-vanilla-milkshake.jpg",
+        "price": 5.49,
+        "category": "Milkshakes",
+        "amount": "0",
+    },
+    {
+        "name": "Double Chocolate Milkshake",
+        "description":
+            "Creamy chocolate ice cream blended with rich chocolate syrup, topped with whipped cream and chocolate shavings.",
+        "img": "../assets/img/dishes/double-chocolate-milkshake.jpg",
+        "price": 5.99,
+        "category": "Milkshakes",
+        "amount": "0",
+    },
+    {
+        "name": "Strawberry Milkshake",
+        "description": "Sweet strawberry ice cream blended with fresh strawberry puree, topped with whipped cream.",
+        "img": "../assets/img/dishes/strawberry-milkshake.jpg",
+        "price": 5.99,
+        "category": "Milkshakes",
+        "amount": "0",
+    },
+    {
+        "name": "Cookies and Cream Milkshake",
+        "description": "Vanilla ice cream blended with crushed chocolate sandwich cookies, topped with whipped cream.",
+        "img": "../assets/img/dishes/cookies-and-cream-milkshake.jpg",
+        "price": 6.49,
+        "category": "Milkshakes",
+        "amount": "0",
+    },
+    {
+        "name": "Peanut Butter Cup Milkshake",
+        "description": "Creamy peanut butter blended with chocolate ice cream and chopped peanut butter cups.",
+        "img": "../assets/img/dishes/peanut-butter-cup-milkshake.jpg",
+        "price": 6.49,
+        "category": "Milkshakes",
+        "amount": "0",
+    },
+    {
+        "name": "New York Cheesecake",
+        "description":
+            "Rich and creamy classic cheesecake on a buttery graham cracker crust, served with berry compote.",
+        "img": "../assets/img/dishes/new-york-cheesecake.jpg",
+        "price": 6.99,
+        "category": "Desserts",
+        "amount": "0",
+    },
+    {
+        "name": "Warm Apple Pie",
+        "description":
+            "Flaky golden pastry filled with warm cinnamon spiced apples, served with a scoop of vanilla ice cream.",
+        "img": "../assets/img/dishes/warm-apple-pie.jpg",
+        "price": 6.49,
+        "category": "Desserts",
+        "amount": "0",
+    },
+    {
+        "name": "Chocolate Brownie Sundae",
+        "description": "Warm fudge brownie topped with vanilla ice cream, hot fudge sauce, and whipped cream.",
+        "img": "../assets/img/dishes/chocolate-brownie-sundae.jpg",
+        "price": 7.49,
+        "category": "Desserts",
+        "amount": "0",
+    },
+    {
+        "name": "Deep Dish Cookie",
+        "description": "Warm chocolate chip skillet cookie topped with vanilla ice cream and caramel drizzle.",
+        "img": "../assets/img/dishes/deep-dish-cookie.jpg",
+        "price": 6.99,
+        "category": "Desserts",
+        "amount": "0",
+    },
+    {
+        "name": "Glazed Donut Trio",
+        "description":
+            "Three warm mini glazed donuts dusted with cinnamon sugar, served with a chocolate dipping sauce.",
+        "img": "../assets/img/dishes/glazed-donut-trio.jpg",
+        "price": 5.49,
+        "category": "Desserts",
+        "amount": "0",
+    },
+];

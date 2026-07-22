fe0c15f2313e59ab664d283ef59f8674f0a25869
wanjamueller <me@wanjamueller.com>
2026-07-22 23:17:40 +0200

    burger container done with placeholder data

---
+++ scripts/templates.js
@@ -0,0 +1,17 @@
+function burgerSectionTemplate() {
+    return /*html*/ `
+            <div class="dish-card">
+                <img src="../assets/img/dishes/classic-bbq-bacon-burger.jpg" alt="">
+                <div class="dish-text-wrapper">
+                    <div class="dish-name">
+                        <h4>Pizza Chorizo</h4>
+                        <p>Tomato slices, Mozzarella and Chorizo</p>
+                    </div>
+                    <div class="dish-price">
+                        <p>price</p>
+                        <button class="add-button">add to basket</button>
+                    </div>
+                </div>
+            </div>
+    `;
+}
